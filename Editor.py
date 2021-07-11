37c09386d6ac27eef450a93e76ae66503e139258
Nathaniel <millernathaniel@gmail.com>
2021-07-11 13:24:10 -0400

    Editor.py Update

--- Editor.py
+++ Editor.py
@@ -2,11 +2,15 @@
 # using Tkinter package
 
 import tkinter as tk
+from tkinter import filedialog
 
 
 class Menubar:
     def __init__(self, parent):
         font_specs = ("times new roman", 14)
+
+        self.master = master
+        self.filename = None
 
         menubar = tk.Menu(parent.master, font=font_specs)
         parent.master.config(menu=menubar)
@@ -40,14 +44,27 @@ class MillyText:
 
         self.menubar = Menubar(self)
 
-    def set_window_title(self):
+    def set_window_title(self, name=None):
         pass
 
     def new_file(self):
         pass
 
     def open_file(self):
-        pass
+        self.filename = filedialog.askopenfilename(defaultextension="txt", filetypes=[("All Files", "*.*"), 
+        ("Text Files", "*.txt"), 
+        ("Python Scripts", "*.py"), 
+        ("Java Files", "*.java"),
+        ("Markdown Documents", "*.md"), 
+        ("JavaScript Files", "*.js"), 
+        ("HTML Documents", "*.html"),
+        ("CSS Documents", "*.css")]
+
+        )
+        if self.filename:
+            self.textarea.delete(1.0, tk.END)
+            with open(self.filename, "r") as f:
+                self.textarea.insert(1.0, f.read())
 
     def save(self):
         pass
