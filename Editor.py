510115f1261539894c11dff26826208d0e07d453
Nathaniel <millernathaniel@gmail.com>
2021-07-10 23:34:06 -0400

    CodeUpdate

--- Editor.py
+++ Editor.py
@@ -1,2 +1,16 @@
 #Source Code will be on this file
 #using Tkinter package
+
+import tkinter as tk
+
+class PyText:
+    def _init_(self, master):
+        master.title("Untitled - PyText")
+        master.geometry("1200x700")
+
+
+
+if __name__ == "__main__":
+    master = tk.Tk()
+    pt = PyText(master)
+    master.mainloop()
